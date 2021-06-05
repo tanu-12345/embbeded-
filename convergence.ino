c941efec8f653d22afdaa853771bde0f07b84acc
tanu-12345 <85399878+tanu-12345@users.noreply.github.com>
2021-06-05 23:56:00 +0530

    Add files via upload

---
+++ convergence.ino
@@ -0,0 +1,31 @@
+void setup() {
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int i=1;
+  for(int j=8; j>=5;j--)
+  {
+    digitalWrite(i,HIGH);
+    digitalWrite(j,HIGH);
+    delay(2000);
+    
+    digitalWrite(i,LOW);
+    digitalWrite(j,LOW);
+    i++;
+    
+    
+  }
+  
+  // put your main code here, to run repeatedly:
+
+}
